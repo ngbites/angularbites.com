de342ddd6ade9fad239d674dff0a3ee03a450131
giancarlo <giancarlopsk@gmail.com>
2020-07-11 23:39:30 +0100

    Improved responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   purge: ['./src/**/*.ejs'],
   theme: {
-    screens: {
-      xl: '650px',
-    },
     fontFamily: {
       sans: ['Martel', 'serif'],
       serif: ['Martel', 'serif'],
     },
     container: {
       center: true,
+      padding: {
+        default: '1rem',
+      },
     },
   },
   variants: {},
