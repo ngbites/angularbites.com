3eca3b6b1e4246448eb979ced998a17209911dd0
giancarlo <giancarlopsk@gmail.com>
2020-07-26 00:42:29 +0100

    More design improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./src/**/*.ejs'],
   theme: {
     fontFamily: {
-      sans: ['Martel', 'serif'],
+      sans: [
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
+        'sans-serif',
+      ],
       serif: ['Martel', 'serif'],
     },
     container: {
